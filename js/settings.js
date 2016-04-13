045e1b195f562dbc3244536ea654d92c5689bb71
Lars Næsbye Christensen <lars@naesbye.dk>
2016-04-13 15:47:15 +0200

    notify user in admin panel

--- js/settings.js
+++ js/settings.js
@@ -15,11 +15,13 @@ function invalidORCID(orcid) {
     // False if orcid has correct form (including hyphens) and valid checksum, else error string
     var orcid_no_hyphens=orcid.replace(/^(\d{4})-(\d{4})-(\d{4})-(\d\d\d[\dX])$/,"$1$2$3$4");
     if (orcid_no_hyphens==orcid) { // will not match if replace succeeded
-	alert("Invalid ORCID, bad form");
+	document.getElementById('orcidstatus').style.color = "red";
+	document.getElementById('orcidstatus').innerHTML = "Invalid ORCID, bad form";
 	return true;
     }
     if (orcid_no_hyphens.charAt(15)!=generateCheckDigit(orcid_no_hyphens)) {
-	alert("Invalid ORCID, bad check digit");
+	document.getElementById('orcidstatus').style.color = "red";
+	document.getElementById('orcidstatus').innerHTML = "Invalid ORCID, bad checksum";
 	return true;
     }
     return false;
@@ -38,7 +40,6 @@ $(document).ready(function() {
 if (!invalidORCID(orcid))
 
 {
-
 		$.ajax(OC.linkTo('user_orcid','ajax/set_orcid.php'), {
 		 	type:"POST",
 		  	data:{
@@ -46,7 +47,8 @@ if (!invalidORCID(orcid))
 		 	},
 		 	dataType:'json',
 		 	success: function(s){
-				// what to do more upon success?
+			document.getElementById('orcidstatus').style.color = "green";
+			document.getElementById('orcidstatus').innerHTML = "Validated and stored. <a href='https://orcid.org/"+orcid+"' target='_blank'> Go to web entry for this ORCID.";
 		 	}
 	});
 
--- templates/settings.tpl.php
+++ templates/settings.tpl.php
@@ -1,5 +1,5 @@
 <fieldset id="orcidSettings" class="section">
 <h2>ORCID Identifier</h2>
 <img src="/apps/user_orcid/img/orcid.png"> <input type='text' name='inputorcid' id='idtextfield' original-title='' title='Input the ORCID identifier you wish to associate with this account.'>
- <input type='submit' value='Save' original-title='' id='idsubmit' title='Validate and store ORCID'>
+ <input type='submit' value='Save' original-title='' id='idsubmit' title='Validate and store ORCID'> <div id='orcidstatus' style="font-size: .8em;"></div>
 </fieldset>
