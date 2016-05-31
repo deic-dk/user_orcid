1dfd926bff3b43998a990446a31b9823c497712a
Lars Næsbye Christensen <lars@naesbye.dk>
2016-05-31 11:36:37 +0200

    text, labels, proper ids et al.

--- templates/settings_admin.tpl.php
+++ templates/settings_admin.tpl.php
@@ -1,4 +1,7 @@
 <fieldset id="orcidAdminSettings" class="section">
-<h2>ORCID</h2>
-<img src="/apps/user_orcid/img/orcid.png">
+<h2><img src="/apps/user_orcid/img/orcid.png"> ORCID - client credentials</h2>
+<label for="inputclientappid">Client App ID: </label><input type='text' name='inputclientappid' id='inputclientappid' original-title='' title='Set the Client app ID for OAuth'><br>
+<label for="inputclientsecret">Client Secret: </label><input type='text' name='inputclientsecret' id='inputclientsecret' original-title='' title='Set the Client secret for OAuth'><br>
+<input type='submit' value='Store OAuth values' original-title='' id='clientsubmit' title='Store OAuth values'>
 </fieldset>
+
--- templates/settings_personal.tpl.php
+++ templates/settings_personal.tpl.php
@@ -1,5 +1,6 @@
 <fieldset id="orcidPersonalSettings" class="section">
-<h2>ORCID</h2>
+<h2>ORCID - Personal</h2>
 <img src="/apps/user_orcid/img/orcid.png"> <input type='text' name='inputorcid' id='idtextfield' original-title='' title='Shows the ORCID identifier associated with this account.' readonly>
  <input type='submit' value='Confirm ORCID' original-title='' id='idsubmit' title='Login, validate, and store ORCID'> <div id='orcidstatus' style="font-size: .8em;"></div>
 </fieldset>
+
