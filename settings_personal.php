0607279695824162ecbdfea044ef541140066f42
deltafunction <frederik@orellana.dk>
2017-01-27 15:14:09 +0100

    Small fix.

--- settings_personal.php
+++ settings_personal.php
@@ -5,7 +5,7 @@ $tmpl = new OCP\Template('user_orcid', 'settings_personal.tpl');
 
 $orcid = \OCP\Config::getUserValue($user, 'user_orcid', 'orcid');
 $tmpl->assign('orcid', $orcid);
-$orcid = \OCP\Config::getUserValue($user, 'user_orcid', 'orcid_token');
+$orcid_token = \OCP\Config::getUserValue($user, 'user_orcid', 'orcid_token');
 $tmpl->assign('orcid_token', $orcid_token);
 
 OCP\Util::addScript('user_orcid', 'settings_personal');
