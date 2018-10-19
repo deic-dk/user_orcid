cab7a80b9205fadab2a00e85d6a55bf61a175f6b
deltafunction <frederik@orellana.dk>
2018-10-19 15:22:56 +0200

    Closed input tags

--- templates/settings_admin.tpl.php
+++ templates/settings_admin.tpl.php
@@ -1,7 +1,7 @@
 <fieldset id="orcidAdminSettings" class="section">
 <h2><img src="<?php echo OC::$WEBROOT;?>/apps/user_orcid/img/orcid.png"> ORCID - client credentials</h2>
-<label for="inputclientappid">Client App ID: </label><input type='text' name='inputclientappid' id='inputclientappid' original-title='' title='Set the Client app ID for OAuth'><br>
-<label for="inputclientsecret">Client Secret: </label><input type='text' name='inputclientsecret' id='inputclientsecret' original-title='' title='Set the Client secret for OAuth'><br>
-<input type='submit' value='Store OAuth values' original-title='' id='clientsubmit' name='clientsubmit' title='Store OAuth values'></input> <div id='clientstatus' style="font-size: .8em;"></div>
+<label for="inputclientappid">Client App ID: </label><input type='text' name='inputclientappid' id='inputclientappid' original-title='' title='Set the Client app ID for OAuth'/><br>
+<label for="inputclientsecret">Client Secret: </label><input type='text' name='inputclientsecret' id='inputclientsecret' original-title='' title='Set the Client secret for OAuth'/><br>
+<input type='submit' value='Store OAuth values' original-title='' id='clientsubmit' name='clientsubmit' title='Store OAuth values'/><div id='clientstatus' style="font-size: .8em;"></div>
 </fieldset>
 
