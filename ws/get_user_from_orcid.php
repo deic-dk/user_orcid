d30114482eabfb260ab6cb1b48af6aeccf152ea8
Frederik Orellana <frederik@orellana.dk>
2023-08-07 13:41:38 +0200

    Support queries from sciencerepository.

--- ws/get_user_from_orcid.php
+++ ws/get_user_from_orcid.php
@@ -2,22 +2,26 @@
 
 OCP\JSON::checkAppEnabled('user_orcid');
 OCP\JSON::checkAppEnabled('files_sharding');
+OCP\JSON::checkAppEnabled('chooser');
 
 $user = OCP\USER::getUser();
 $allowedQueryUser = trim(\OCP\Config::getSystemValue('vlantrusteduser', ''));
 
 // This is to allow ScienceRepository/Zenodo to query for user matching orcid
-if((empty($user) || $user!=$allowedQueryUser) && !OCA\FilesSharding\Lib::checkIP()){
-	http_response_code(401);
-	exit;
+if(empty($user)){
+	$user = \OC_Chooser::checkIP();
+	if(empty($user) || $user!=$allowedQueryUser){
+		http_response_code(401);
+		exit;
+	}
 }
 
 require_once('user_orcid/lib/lib_orcid.php');
 
 $orcid = isset($_REQUEST['orcid'])?$_REQUEST['orcid']:'';
 
-$user = OCA\FilesOrcid\Lib::dbGetUserFromOrcid($orcid);
+$lookedUpUser = OCA\FilesOrcid\Lib::dbGetUserFromOrcid($orcid);
 
-\OCP\Util::writeLog('user_group_admin', 'Returning user '.$user, \OC_Log::WARN);
+\OCP\Util::writeLog('user_group_admin', 'Returning user '.$lookedUpUser, \OC_Log::WARN);
 
-OCP\JSON::encodedPrint($user);
+OCP\JSON::encodedPrint($lookedUpUser);
